8c5a07af08e7ab5da5fca58b185784e2f94289df
webdibb <59144185+webdibb@users.noreply.github.com>
2020-07-13 21:42:42 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Calculator from './layout/Calculator/Calculator';
+
+import "./styles/styles.css"
+
+const App = () => <div className="app"><Calculator /></div>
+
+export default App
---
+++ src/layout/Calculator/Calculator.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const Calculator = () => <main className="calculator"></main>
+
+export default Calculator
